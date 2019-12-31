9161b1135217085eb3afadcaeb30896f48990be2
Antonio Del Cogliano <delchi1199@gmail.com>
2019-12-31 17:02:38 +0100

    Update Interface2.py

--- Interface2.py
+++ Interface2.py
@@ -1,25 +1,25 @@
 import socket
 import fcntl
 import struct
+import os
+import I2C_LCD_driver
 import netifaces as ni
 from time import sleep
-import I2C_LCD_driver
-from  Test import *
-from LCD2 import *
 from RPi import GPIO
+from Test import *
+from LCD2 import *
 from Sensor import *
-#temporary variables
-sensorState = 1 #state of distance sensor
-plateTemp = 43 #heatbed temperature
-boxTemp = 34 #box temperature
-printerState = "ON"
-fanState = "ON"
-lightState = "ON"
-camState = "ON"
-buzzerState = "ON"
-customval = 60 #value of custom material temperature
+from Relay import *
+from Serial import *
+from Automate import *
+from Notifications import*
+
+#temporary variables, queste varabili verranno sostituite con l'implementazione di tutto l'hardware necessario
+
+boxTemp = 34 #box max temperature
 pTimeH = 1 #printing time (hours)
 pTimeM = 10 #printing time (minutes)
+maxBoxTemp = 34
 
 clk = 17
 dt = 18
@@ -29,42 +29,44 @@ Trigger = 23
 Echo = 24
 MaxDistance = 300
 
+class VariableValue: #un valore variabile può essere settato ad un nuovo stato o letto al suo stato precedente
+    def __init__(self, value):
+        self.value = value
+
+    def set(self, newValue):
+        self.value = newValue
+
+    def check(self):
+        return self.value
+
+#liste per le varie schermate
 list1 = ["  ON/OFF MENU", "  SETTINGS", "  MATERIAL SETTINGS", "  SHUTDOWN"]  #menuscreen list
+staticList2 = ["   PRINTER", "   FAN", "   CAM  ", "   LIGHT ", "   BUZZER"] #staticOnoffscreen list
+staticList3 = ["  FIL. ALERT ", "  TEMP. ALERT ", "  ENDED PRINT ", voidstring()] #settingscreen list
+staticList4 = ["  PLA (TEMP 50)", "  ABS (TEMP 80)", voidstring(), voidstring()] #materialscreen list
+List5 = ["TURN OFF THE SYSTEM?", "        SURE        ", voidstring(), "         NO         "] #shutdownscreen list
 
-list5 = ["TURN OFF THE SYSTEM?", "        SURE        ", voidstring(), "         NO         "] #shutdownscreen list
-
-def staticOnoffString(): #onoffString function put the State0-State4 value into the onoffscreen list
-    staticList2 = ["   PRINTER", "   FAN", "   CAM  ", "   LIGHT ", "   BUZZER"] #staticOnoffscreen list
-    return staticList2
-
-def staticSettingsString(): #settingsString function put value into the settingsscreen list
-    list3 = ["  FIL. ALERT ", "  TEMP. ALERT ", "  ENDED PRINT ", voidstring()] #settingscreen list
-    return list3
-
-def materialscreenString(selection, customval): #materialscreenString function chooses different list according to "selection" val
-
-    list4a = ["=> PLA (H 50)", "   ABS (H 80)", "   CUSTOM (H " + str(customval) + ")"]
-    list4b = ["   PLA (H 50)", "=> ABS (H 80)", "   CUSTOM (H " + str(customval) + ")"]
-    list4c = ["   PLA (H 50)", "   ABS (H 80)", "=> CUSTOM (H " + str(customval) + ")"]
-
-    materialStringList = []
-
-    if (selection == "PLA"):
-        materialStringList = list4a
-    if (selection == "ABS"):
-        materialStringList = list4b
-    if (selection == "CUSTOM"):
-        materialStringList = list4c
-
-    return materialStringList
-
+#oggetti sensore
 Encoder = Rotary(dt, clk, sw)
 distanceSensor = Sensor(Trigger, Echo, MaxDistance)
+
+#oggetti schermate(Menu, DynamicMenu)
 menuscreen = Menu(list1)
-onoffscreen = DynamicMenu(staticOnoffString())
-settingscreen = DynamicMenu(staticSettingsString())
-materialscreen = Menu(materialscreenString("PLA", customval))
-shutdownscreen = Menu(list5)
+onoffscreen = DynamicMenu(staticList2)
+settingscreen = DynamicMenu(staticList3)
+materialscreen = DynamicMenu(staticList4)
+shutdownscreen = Menu(List5)
+
+#oggetti flag
+minBedTemp = VariableValue(18) #la temperatura minima del piatto è settata a 18 (prova)
+printerState = VariableValue("OFF")
+fanState = VariableValue("OFF")
+camState = VariableValue("OFF")
+lightState = VariableValue("OFF")
+buzzerState = VariableValue("OFF")
+filAlert = VariableValue("OFF")
+tempAlert = VariableValue("OFF")
+endedPrint = VariableValue("OFF")
 
 def back():
     if distanceSensor.readDistanceCentimeters() < 20:
@@ -77,17 +79,25 @@ def stdbyScreen(temp1, temp2, State0, State1, State2, State3):
     stdbyScreen.display()
 
 def mainScreen(hours, minutes):
-    list0 = ["  PRINTER  CONTROL  ", voidstring(), " PRINTING TIME "+str(hours)+":"+str(minutes),"   IP "+  IpAdress()+" "]
+    list0 = ["  PRINTER  CONTROL  ", voidstring(), " PRINTING TIME "+str(hours)+":"+str(minutes)," IP "+  IpAdress()+" "]
     mainscreen = Screen(list0)
     mainscreen.display()
 
 def runInterface():
+    turnOffAll()
     while True:
-        stdbyScreen(plateTemp, boxTemp, printerState, lightState, camState, buzzerState)
+
+        Controller = TemperatureController("heatbed", "fan", minBedTemp.check(), maxBoxTemp)#viene creato un nuovo controller delle temperature
+        plateTemp = getTemp() #heatbed temperature
+        Controller.checkAndEnable(plateTemp, boxTemp)
+        stdbyScreen(plateTemp, boxTemp, printerState.check(), lightState.check(), camState.check(), buzzerState.check())#la schermata di standby è creata e aggiornata
+        print ("executing the non-interactive block")
+
         if back() == True:#checking sensor
             clear()
             sleep(1)
             while True:
+                    print("the interactive execution has started")
                 #mainscreen running
                     mainScreen(1, 10)
 
@@ -108,7 +118,7 @@ def runInterface():
                             #menuscreen selection block 0
                             if menuscreen.read(Encoder.button(), cursor)== 0:
                                 cursor = 0
-                                dynamicList = ["OFF", "OFF", "OFF", "OFF", "OFF"]
+                                dynamicList = [printerState.check(), fanState.check(), camState.check(), lightState.check(), buzzerState.check()]
                                 clear()
                                 Encoder.enable()
                                 sleep(0.5)
@@ -125,45 +135,57 @@ def runInterface():
                                     if onoffscreen.read(Encoder.button(), cursor) == 0:
                                         if dynamicList[0] == "OFF":
                                             dynamicList[0] = "ON "
+                                            printerState.set("ON ")
+                                            #setPrinter(1)
 
                                         elif dynamicList[0] == "ON ":
                                             dynamicList[0] = "OFF"
+                                            #setPrinter(0)
+                                            printerState.set("OFF")
 
                                         onoffscreen.run(dynamicList, cursor)
 
                                     if onoffscreen.read(Encoder.button(), cursor) == 1:
                                         if dynamicList[1] == "OFF":
                                             dynamicList[1] = "ON "
+                                            fanState.set("ON ")
 
                                         elif dynamicList[1] == "ON ":
                                             dynamicList[1] = "OFF"
+                                            fanState.set("OFF")
 
                                         onoffscreen.run(dynamicList, cursor)
 
                                     if onoffscreen.read(Encoder.button(), cursor) == 2:
                                         if dynamicList[2] == "OFF":
                                             dynamicList[2] = "ON "
+                                            camState.set("ON ")
 
                                         elif dynamicList[2] == "ON ":
                                             dynamicList[2] = "OFF"
+                                            camState.set("OFF")
 
                                         onoffscreen.run(dynamicList, cursor)
 
                                     if onoffscreen.read(Encoder.button(), cursor) == 3:
                                         if dynamicList[3] == "OFF":
                                             dynamicList[3] = "ON "
+                                            lightState.set("ON ")
 
                                         elif dynamicList[3] == "ON ":
                                             dynamicList[3] = "OFF"
+                                            lightState.set("OFF")
 
                                         onoffscreen.run(dynamicList, cursor)
 
                                     if onoffscreen.read(Encoder.button(), cursor) == 4:
                                         if dynamicList[4] == "OFF":
                                             dynamicList[4] = "ON "
+                                            buzzerState.set("ON ")
 
                                         elif dynamicList[4] == "ON ":
                                             dynamicList[4] = "OFF"
+                                            buzzerState.set("OFF")
 
                                         onoffscreen.run(dynamicList, cursor)
 
@@ -173,7 +195,7 @@ def runInterface():
                             #menuscreen selection block 1
                             if menuscreen.read(Encoder.button(), cursor)== 1:
                                 cursor = 0
-                                dynamicList = ["OFF", "OFF", "OFF", "   "]
+                                dynamicList = [filAlert.check(), tempAlert.check(), endedPrint.check(), "   "]
                                 clear()
                                 Encoder.enable()
                                 sleep(0.5)
@@ -188,43 +210,72 @@ def runInterface():
                                     if settingscreen.read(Encoder.button(), cursor) == 0:
                                         if dynamicList[0] == "OFF":
                                             dynamicList[0] = "ON "
+                                            filAlert.set("ON ")
 
                                         elif dynamicList[0] == "ON ":
                                             dynamicList[0] = "OFF"
+                                            filAlert.set("OFF")
 
                                         settingscreen.run(dynamicList ,cursor)
 
                                     if settingscreen.read(Encoder.button(), cursor) == 1:
                                         if dynamicList[1] == "OFF":
                                             dynamicList[1] = "ON "
+                                            tempAlert.set("ON ")
 
                                         elif dynamicList[1] == "ON ":
                                             dynamicList[1] = "OFF"
+                                            tempAlert.set("OFF")
 
                                         settingscreen.run(dynamicList ,cursor)
 
                                     if settingscreen.read(Encoder.button(), cursor) == 2:
                                         if dynamicList[2] == "OFF":
                                             dynamicList[2] = "ON "
+                                            endedPrint.set("ON ")
 
                                         elif dynamicList[2] == "ON ":
                                             dynamicList[2] = "OFF"
+                                            endedPrint.set("OFF")
 
                                         settingscreen.run(dynamicList ,cursor)
 
 
                             #menuscreen selection block 2
                             if menuscreen.read(Encoder.button(), cursor)== 2:
+                                dynamicList = ["   ", "   ", "   ", "   "]
                                 clear()
                                 Encoder.enable()
                                 cursor = 0
                                 while True:
                                     if Encoder.read()!= None:
                                         cursor = Encoder.read()
-                                        materialscreen.run(cursor)
+                                        materialscreen.run(dynamicList, cursor)
 
                                     if back() == True:
                                         runInterface()
+
+                                    if materialscreen.read(Encoder.button(), cursor) == 0:
+                                        if dynamicList[0] == "   ":
+                                            dynamicList[0] = "<=="
+                                            dynamicList[1] = "   "
+                                            minBedTemp.set(25)
+
+                                        elif dynamicList[0] == "<==":
+                                            dynamicList[0] = "   "
+
+                                        materialscreen.run(dynamicList ,cursor)
+
+                                    if materialscreen.read(Encoder.button(), cursor) == 1:
+                                        if dynamicList[1] == "   ":
+                                            dynamicList[1] = "<=="
+                                            dynamicList[0] = "   "
+                                            minBedTemp.set(10)
+
+                                        elif dynamicList[1] == "<==":
+                                            dynamicList[1] = "   "
+
+                                        materialscreen.run(dynamicList ,cursor)
 
                             #menuscreen selection block 3
                             if menuscreen.read(Encoder.button(), cursor)== 3:
@@ -249,7 +300,16 @@ def runInterface():
                                         shutdownscreen.run(cursor)
 
                                     if shutdownscreen.read(Encoder.button(), cursor) == 1:
-                                        print("shutdown")
+                                        clear()
+                                        mylcd.lcd_display_string("  SHUTTING DOWN", 2, 0)
+                                        i = 0
+                                        while i < 3:
+                                            mylcd.lcd_display_string(".", 2, 15 + i)
+                                            i = i + 1
+                                            sleep(1)
+                                        clear()
+                                        mylcd.backlight(0)
+                                        os.system("shutdown -h now");
 
                                     if shutdownscreen.read(Encoder.button(), cursor) == 3:
                                         clear()
